cd1ef92a8baca4834e62b76d8645064680c3d1b6
fotomain <foto888999@gmail.com>
2023-01-31 16:02:53 +0200

    s

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,6 +30,49 @@ import "swiper/swiper-bundle.css";
 const Home: React.FC = () => {
 
   const blur=0
+  interface Text_xy_type {
+    top?:string,
+    left?:string,
+    right?:string,
+    marginTop?:string,
+    fontSize?:string,
+    paddingLeft?:string,
+    marginLeft?:string,
+  }
+
+
+  const text_xy_design1 : Text_xy_type[] =[
+      // 000 UP contatcts
+      {marginTop: '50%', left: '35%'   , fontSize: '25px', marginLeft:'30pox'},
+      // {margin_top: '50%', left: '35%'   , font_size: '25px'},
+      // 111 MAIN
+      {marginTop: '50%', left: '5%'    , fontSize: '25px'},
+      // 222
+      {marginTop: '-80%', left: '75%'   , fontSize: '15px'},
+  ]
+
+
+  const text_xy_design2 : Text_xy_type[] =[
+      // 000 UP
+      {top: '45%', right: '20px' , fontSize: '25px'},
+      // 111 MAIN
+      {marginTop: '70%', left: '5%'    , fontSize: '30px'},
+      // 222
+      {marginTop: '65%', left: '65%'   , fontSize: '25px'},
+  ]
+
+  const text_xy_design3 : Text_xy_type[] =[
+      // 000 UP
+      {marginTop: '50%', left: '45%'   , fontSize: '15px'},
+      // 111 MAIN
+      {marginTop: '80%', left: '5%'    , fontSize: '15px'},
+      // 222
+      {marginTop: '80%', left: '75%'   , fontSize: '15px'},
+  ]
+
+
+  const [design_number, set_design_number] = useState<number>(1);
+  const [text_xy, set_text_xy] = useState(text_xy_design1);
 
   const [messages, setMessages] = useState<Message[]>([]);
 
@@ -46,6 +89,22 @@ const Home: React.FC = () => {
 
 
   useEffect(() => {
+
+    if(design_number==1) {
+      set_text_xy(text_xy_design1)
+    }
+    if(design_number==2) {
+      set_text_xy(text_xy_design2)
+    }
+    if(design_number==3) {
+      set_text_xy(text_xy_design3)
+    }
+
+    return () => {}
+
+  }, [design_number]);
+
+  useEffect(() => {
     const interval = setInterval(() => {
       if(navigationNextRef) {
         navigationNextRef?.current?.swiper?.slideNext();
@@ -58,8 +117,8 @@ const Home: React.FC = () => {
 
   const data = [
     {
-      title1:'tel. 2999888',
-      title2:'RIMI blakus',
+      title1:'blakus 500 m',
+      title2:'RIMI vienmer',
       title3:'atlaide 30%',
       video: "https://joy1.videvo.net/videvo_files/video/free/video0467/large_watermarked/_import_615435000f6eb2.81789495_preview.mp4",
 
@@ -68,22 +127,17 @@ const Home: React.FC = () => {
       title1:'www.win.lv',
       title2:'ZIEMAS RIEPAS',
       title3:'ŠODIEN -25%',
-      video: "https://joy1.videvo.net/videvo_files/video/free/2013-09/large_watermarked/DiscoLights2Videvo_preview.mp4"
+      video: "https://media.istockphoto.com/id/1370751696/video/futuristic-smart-traffic.mp4?s=mp4-640x640-is&k=20&c=gvjqduw5xHFfvxMjFyLkNvCjTwn10CqCk4qI0s6lX_4="
+      // video: "https://joy1.videvo.net/videvo_files/video/free/2013-09/large_watermarked/DiscoLights2Videvo_preview.mp4"
 
     },
     {
-      title1:'blakus 500 m',
+      title1:'tel. 2999888',
       title2:'SPA / PIRTS',
       title3:'-30% no 18:00 ',
-      video: "https://joy1.videvo.net/videvo_files/video/free/2022-01/large_watermarked/211212_02_Jakarta_4k_018_preview.mp4"
+      video: "https://joy.videvo.net/videvo_files/video/premium/video0249/large_watermarked/4000-2_05438_preview.mp4"
 
     },
-    // {
-    //   title1:'title111111',
-    //   title2:'title222222',
-    //   title3:'title333333',
-    //   video: "https://joy1.videvo.net/videvo_files/video/free/2013-12/large_watermarked/CROWD_JUMPS_LIGHTS_PULSE_preview.mp4"
-    // },
   ]
 
   const navigationNextRef = useRef() as any
@@ -171,17 +225,25 @@ const Home: React.FC = () => {
 
 
                     <div
-                        style={{
+                        style={{...text_xy[0],...{
                             // height:'40px', width:'100px',
                             backgroundColor:'yellow',
-                            // 111
+                            // 000
 
                             // alignSelf:'start',
-                            top:'60px',
-                            right:'20%',
+                            // top:'60px',
+                            // marginTop:text_xy[0].margin_top,
+                            // top:text_xy[0].top,
+                            // // right:'20%',
+                            // right:text_xy[0].right,
+                            //
+                            // fontSize: text_xy[0].font_size,
+                                //'25px',
+
+                            marginLeft:'85px',
                             padding:'5px',
                             color:'red',
-                            fontSize:'25px',
+
                             fontWeight:'bold',
 
 
@@ -192,21 +254,26 @@ const Home: React.FC = () => {
                             flexDirection:'row',
                             justifyContent:'center',
                             alignItems:'center',
-                        }}
+                        }}}
+
                     >
                       {card.title1}
                     </div>
 
                           <div
-                            style={{
+                              style={{...text_xy[1],...{
                                 //  height:'40px', width:'100px',
                                 backgroundColor:'yellow',
-                            // 222
-                            marginTop:'-10%',
-                            left:'15%',
-                                padding:'10px',
+                            // 111
+
+                              // marginTop:text_xy[1].margin_top,
+                              // left:text_xy[1].left,
+                              // fontSize: text_xy[1].font_size,
+                              //
+
+                              padding:'10px',
                                 color:'red',
-                                fontSize:'30px',
+                                // fontSize:'30px',
                                 fontWeight:'bold',
 
                               position:'absolute',
@@ -216,21 +283,24 @@ const Home: React.FC = () => {
                                 flexDirection:'row',
                                 justifyContent:'center',
                                 alignItems:'center',
-                            }}
+                            }}}
                           >
                             {card.title2}
                           </div>
 
                                   <div
-                                      style={{
+                                      // 222
+                                      style={{...text_xy[2],...{
                                           // height:'40px', width:'100px',
                                           backgroundColor:'yellow',
-                                        marginTop:'100px',
-                                        left:'20%',
+
+                                        // top:text_xy[2].margin_top,
+                                        // left:text_xy[2].left,
+                                        // fontSize: text_xy[2].font_size,
 
                                           padding:'5px',
                                           color:'red',
-                                          fontSize:'25px',
+                                          // fontSize:'25px',
 
                                           position:'absolute',
                                           zIndex:2,
@@ -240,7 +310,7 @@ const Home: React.FC = () => {
                                           justifyContent:'center',
                                           alignItems:'center',
 
-                                      }}
+                                      }}}
                                   >
                                     {card.title3}
                                   </div>
